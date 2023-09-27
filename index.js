8d16b1ad6fc9a528b1c57e2d74102af63d27289d
cristhianmeza <aslheycaso92@gmail.com>
2023-09-27 16:59:24 -0500

    mi tercer commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
+const ov1 = document.getElementById("nombre")
+const presultado = document.getElementById("resultado")
 
-function Calcular(){
-console.log("Hola Cris")
+function calcular(){
 
+    const rval1= ov1.value
+
+ console.log(rval1)
+    presultado.innerText= rval1
 }
+
